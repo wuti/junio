5305bd42b21c2dff8a2781efff9831459bc25072
Steve Missing <nevermissing@vip.qq.com>
2013-03-05 10:46:39 +0800

    No debug.

--- src/main/java/com/foo/example/action/HelloAction.java
+++ src/main/java/com/foo/example/action/HelloAction.java
@@ -54,7 +54,7 @@ public class HelloAction extends FooGenericAction { // 模型驱动
 		// fooService.remove(foo);
 		//
 		// System.out.println("enter2");
-		System.out.println("hehe");
+		System.out.println("a5");
 
 		Map<String, Object> myMap = Maps.newHashMap();
 		Search search = new Search();
